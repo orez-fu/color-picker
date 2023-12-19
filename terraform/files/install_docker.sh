a862eb6b95630b2c7ea36238c4d1d84e40ba314b
Hoang Phu <phuhoang.c2@gmail.com>
2023-12-20 03:55:10 +0700

    update

--- terraform/files/install_docker.sh
+++ terraform/files/install_docker.sh
@@ -4,6 +4,7 @@ apt install apt-transport-https ca-certificates curl software-properties-common 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" -y
 apt install docker-ce -y
+usermod -aG docker jenkins
 usermod -aG docker ubuntu
 
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
--- terraform/files/setup_kubernetes.sh
+++ terraform/files/setup_kubernetes.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-echo "hello" >> /home/ubuntu/1.txt
 curl -sfL https://get.k3s.io | sh -
 chmod 644 /etc/rancher/k3s/k3s.yaml 
 echo export "KUBECONFIG=/etc/rancher/k3s/k3s.yaml" >> /home/ubuntu/.bashrc
